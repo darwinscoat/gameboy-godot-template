007b0d0ff31df5a3f2dc007064aa4486573b79ef
Mitch <mitchell.boulter@googlemail.com>
2026-09-16 20:16:51 +0100

    fix: fix for web

--- audio/music.gd
+++ audio/music.gd
@@ -4,39 +4,50 @@ extends Node
 @export var silence_db: float = -40.0
 
 var current = ""
-var positions = {}
 var fades = {}
 
 
-func play(name, from := 0.0):
+func play(name):
 	if name == current:
 		return
 	fade_out()
 	var player = get_node_or_null(name.to_pascal_case())
 	if player:
-		if fades.has(name):
-			fades[name].kill()
-		player.volume_db = silence_db
-		player.play(from)
-		create_tween().tween_property(player, "volume_db", 0.0, fade_time)
-	current = name
+		player.stream_paused = false
+		player.play()
+		fade_in(name, player)
 
 
 func resume(name):
-	play(name, positions.get(name, 0.0))
+	var player = get_node_or_null(name.to_pascal_case())
+	if player == null or not player.stream_paused:
+		play(name)
+		return
+	if name == current:
+		return
+	fade_out()
+	player.stream_paused = false
+	fade_in(name, player)
 
 
 func stop():
 	fade_out()
 
 
+func fade_in(name, player):
+	if fades.has(name):
+		fades[name].kill()
+	player.volume_db = silence_db
+	create_tween().tween_property(player, "volume_db", 0.0, fade_time)
+	current = name
+
+
 func fade_out():
 	if current == "":
 		return
 	var player = get_node(current.to_pascal_case())
-	positions[current] = player.get_playback_position()
 	var tween = create_tween()
 	tween.tween_property(player, "volume_db", silence_db, fade_time)
-	tween.tween_callback(player.stop)
+	tween.tween_callback(func(): player.stream_paused = true)
 	fades[current] = tween
 	current = ""
